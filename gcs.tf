23ff8d8c791c25a0976d56ec6fc5aaa308c77969
Mike Sirs <6318613@gmail.com>
2020-03-11 21:08:53 +0300

    fixed zone_id for cf dns rec

--- gcs.tf
+++ gcs.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_record" "www-cncfminsk-io" {
   zone_id = "${lookup(data.cloudflare_zones.cncfminsk-io.zones[0], "id")}"
-  name    = "www"
+  name    = "www.cncfminsk.io"
   value   = "c.storage.googleapis.com"
   type    = "CNAME"
   proxied = true
@@ -21,7 +21,7 @@ resource "google_storage_bucket" "www-cncfminsk-io" {
 
 resource "cloudflare_record" "dev-cncfminsk-io" {
   zone_id = "${lookup(data.cloudflare_zones.cncfminsk-io.zones[0], "id")}"
-  name    = "dev"
+  name    = "dev.cncfminsk.io"
   value   = "c.storage.googleapis.com"
   type    = "CNAME"
   proxied = true
